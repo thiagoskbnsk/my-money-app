5210a5410b6455eb63ebcb445b549fcf7cec8be0
Thiago Skibinski <thiagoskibinski@gmail.com>
2017-09-19 21:29:35 -0300

    Colocando status no itemlist

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -31,7 +31,7 @@ class BillingCycleForm extends Component {
             label='Ano' cols='12 4' placeholder='Informe o ano' />
           <Summary credit={sumOfCredits} debt={sumOfDebts}/>
           <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos' />
-          <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Débitos' />
+          <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Débitos' showStatus={true}/>
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>
